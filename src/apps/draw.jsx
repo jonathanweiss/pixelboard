da3189d073ea293a236ccf6001052aa3dbaebdfb
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-09-09 14:48:38 +0200

    Add onClick handler to toggle LEDs

--- src/apps/draw.jsx
+++ src/apps/draw.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import Grid from '../components/grid.jsx';
-import { drawColumn } from '../utils/grid_helper';
+import Column from '../components/column.jsx';
+import Row from '../components/row.jsx';
+import Led from '../components/led.jsx';
+
 import { rows, columns } from '../utils/configuration';
 
 class Draw extends React.Component {
@@ -10,6 +13,14 @@ class Draw extends React.Component {
   constructor() {
     super();
     this.state = { board: this.createEmptyBoard() };
+  }
+
+  toggleLed(col, row, led) {
+    const board = Object.assign([], this.state.board);
+    const isOn = board[col][row][led].isActive;
+    board[col][row][led].isActive = !isOn;
+
+    this.setState({ board });
   }
 
   createEmptyBoard() {
@@ -21,6 +32,7 @@ class Draw extends React.Component {
           width: 40,
           height: 40,
           backgroundColor: '#f5f5f5',
+          color: '0, 143, 0',
           margin: 2,
         };
       }
@@ -34,7 +46,15 @@ class Draw extends React.Component {
     return (
       <div>
         <h2>Draw as you like!</h2>
-        <Grid>{board.map((character, index) => drawColumn(character, index))}</Grid>
+        <Grid>{board.map((column, columnIndex) => {
+          return (<Column key={`column_${columnIndex}`}>
+            {column.map((row, rowIndex) => {
+              return (<Row key={`row_${rowIndex}`}>{row.map((led, ledIndex) => {
+                return <div key={`led_${ledIndex}`} onClick={() => { this.toggleLed(columnIndex, rowIndex, ledIndex); }}><Led key={`led_${ledIndex}`} {...led} /></div>;
+              })}</Row>);
+            })}
+          </Column>);
+        })}</Grid>
 
         <Link to="/">Back</Link>
       </div>
