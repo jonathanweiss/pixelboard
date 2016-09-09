a8d67cc8022df818d1bc00462e42f9aeedaddaf0
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-09-09 14:38:49 +0200

    Display empty board on startup

--- src/apps/draw.jsx
+++ src/apps/draw.jsx
@@ -2,18 +2,39 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import Grid from '../components/grid.jsx';
-import { drawCharacter, getBits } from '../utils/grid_helper';
-import { defaultGrid } from '../utils/configuration';
+import { drawColumn } from '../utils/grid_helper';
+import { rows, columns } from '../utils/configuration';
 
 class Draw extends React.Component {
 
-  render() {
-    const grid = [];
+  constructor() {
+    super();
+    this.state = { board: this.createEmptyBoard() };
+  }
 
+  createEmptyBoard() {
+    const board = [[]];
+    for (let i = 0; i < rows; i++) {
+      board[0][i] = [];
+      for (let k = 0; k < columns; k++) {
+        board[0][i][k] = {
+          width: 40,
+          height: 40,
+          backgroundColor: '#f5f5f5',
+          margin: 2,
+        };
+      }
+    }
+
+    return board;
+  }
+
+  render() {
+    const { board } = this.state;
     return (
       <div>
         <h2>Draw as you like!</h2>
-        <Grid>{grid.map((character, index) => drawCharacter(character, index))}</Grid>
+        <Grid>{board.map((character, index) => drawColumn(character, index))}</Grid>
 
         <Link to="/">Back</Link>
       </div>
